f354a6bd3f726dc26594a4b547bbd8cc003476f3
strengur <steingrimur@740.is>
2018-10-05 04:50:41 +0000

    minor

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		<footer>
 			<h4>&copy;2014-<?php echo date("Y"); ?> | 740.is | Allur&nbsp;réttur áskilinn.</h4>
 		</footer>
-		<script>
+		<!-- <script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -10,7 +10,7 @@
 		ga('create', 'UA-66220009-2', 'auto');
 		ga('send', 'pageview');
 
-		</script>
+		</script> -->
 	<?php wp_footer(); ?>
 	</body>
 </html>
